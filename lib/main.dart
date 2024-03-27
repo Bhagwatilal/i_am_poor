aabb0f851990b7014371fae2670fb1a374e36217
Bhagwatilal <bhagavtilaljoshi1@gmail.com>
2024-03-27 12:30:30 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: Text("I Am Poor"),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+      ),
+    ),
+  );
+}
